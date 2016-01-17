1bdc07f160d5804aacc7dcefafdac1ac9a853d4e
lokeshmeher <lokeshmeher34@gmail.com>
2016-01-18 02:32:15 +0530

    Update cmd_properties.bat

--- cmd_properties.bat
+++ cmd_properties.bat
@@ -17,7 +17,7 @@ doskey gcc = @echo Compiling using -std=c11 -Wall -pedantic $T gcc -std=c11 -Wal
 doskey chrome = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
 
 :: Environment variables
-setx %NEWVAR%=SOMETHING
+:: setx path = "%path%;path\to\new\dir"
 
 :: control flow commands: for, if, goto, call, pause.
 :: to know more type 'help <command>' for more info.
